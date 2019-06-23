8ffe58e149184f5ff5e00a633503dc3685a68f6a
oMMiao <ommaio@foamail.com>
2019-06-23 18:46:04 +0800

    优化时间为TextClock可实时刷新，日期星期半小时刷新一次。

--- app/src/main/java/me/iacn/mbestyle/ui/widget/AppClockWidget.java
+++ app/src/main/java/me/iacn/mbestyle/ui/widget/AppClockWidget.java
@@ -1,17 +1,13 @@
 package me.iacn.mbestyle.ui.widget;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
 import android.widget.RemoteViews;
 
 import java.util.Calendar;
-import java.util.Date;
 
 import me.iacn.mbestyle.R;
-import me.iacn.mbestyle.service.ClockService;
 
 /**
  * Implementation of App Widget functionality.
@@ -24,21 +20,12 @@ public class AppClockWidget extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock);
         updateViews(views);
-        views.setOnClickPendingIntent(R.id.clock, PendingIntent.getService(context, 0, new Intent(context, ClockService.class), 0));
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     private static void updateViews(RemoteViews views) {
         Calendar calendar = Calendar.getInstance();
-        int h = calendar.get(Calendar.HOUR_OF_DAY);
-        String hour = h >= 10 ? String.valueOf(h) : "0" + h;
-        String hour0 = hour.substring(0, 1);
-        String hour1 = hour.substring(1, 2);
-        int m = calendar.get(Calendar.MINUTE);
-        String minute = m >= 10 ? String.valueOf(m) : "0" + m;
-        String minute0 = minute.substring(0, 1);
-        String minute1 = minute.substring(1, 2);
         int y = calendar.get(Calendar.YEAR);
         String year = String.valueOf(y);
         int M = calendar.get(Calendar.MONTH) + 1;
@@ -48,10 +35,6 @@ public class AppClockWidget extends AppWidgetProvider {
         String date = month + "." + day + "." + year;
         int w = calendar.get(Calendar.DAY_OF_WEEK) - 1;
         String week = getDisplayWeek(w);
-        views.setTextViewText(R.id.hour0, hour0);
-        views.setTextViewText(R.id.hour1, hour1);
-        views.setTextViewText(R.id.minute0, minute0);
-        views.setTextViewText(R.id.minute1, minute1);
         views.setTextViewText(R.id.date, date);
         views.setTextViewText(R.id.week, week);
     }
@@ -112,18 +95,6 @@ public class AppClockWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-        context.startService(new Intent(context, ClockService.class));
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-        context.stopService(new Intent(context, ClockService.class));
     }
 }
 
